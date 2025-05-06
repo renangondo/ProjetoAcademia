70924b658ebb8d9688345b0fdddd43f687096b9b
renangondo <renanluizgondo@gmail.com>
2025-05-06 18:04:33 -0300

    criação da pasta static

--- paginas/urls.py
+++ paginas/urls.py
@@ -5,5 +5,5 @@ from .views import indexView
 # path('endereço', minhaView.as_view(), name='nome_da_view')
 
 urlpatterns = [
-    path('inicio/', indexView.as_view(), name='inicio'), 
+    path('', indexView.as_view(), name='inicio'), 
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,4 @@
 from django.views.generic import TemplateView
 
 class indexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'paginas/index.html'
