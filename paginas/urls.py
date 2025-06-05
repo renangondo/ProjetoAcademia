93a815da817d18599ad8a91d1d3749258b6c37fe
RG <RG_gmail.com>
2025-06-05 20:46:21 -0300

    Mudando o template

--- paginas/urls.py
+++ paginas/urls.py
@@ -5,5 +5,5 @@ from .views import IndexView
 # path('endereço', minhaView.as_view(), name='nome_da_view')
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='inicio'), 
+    path('', IndexView.as_view(), name='index'), 
 ]
